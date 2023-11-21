a4e8084ab8b431a53b218cb35b5f66197779f352
Rohan Gajurel <rohangajurel18@gmail.com>
2023-11-21 17:16:50 +0545

    New thing updated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import  React from "react";
 import ReactDom from  "react-dom";
 function Hello()
 {
-    return <h1>Hello World!</h1>;
+    return <h1>Hello </h1>;
  }
   
   ReactDom.render(<Hello/>,document).getElementById("root")
